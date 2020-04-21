efb1019f64ace0013399a894d48850da4ebdc612
github username <github email address>
2020-04-21 16:40:27 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
-
-
+class Book
+    attr_reader :page_count,:author,:genre,:title
+  attr_writer :page_count,:author,:genre
+    def initialize(title)
+        @title = title  
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+      end
+end
+Book.new("And Then There Were None")
