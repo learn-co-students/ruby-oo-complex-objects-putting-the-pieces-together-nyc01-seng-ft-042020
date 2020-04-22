7f741ac2b2eddbbc882bed5a035e02af127c4ef7
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-04-21 20:10:39 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -12,20 +12,3 @@ class Book
  end
 
 end
-
-class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
-  def initialize(brand, color=nil, size=nil, material=nil, condition=nil)
-   @brand = brand
-   @color = color
-   @size = size
-   @material = material
-   @condition = condition
-  end
-
-  def cobble
-    @condition = "new"
-   puts "Your shoe is as good as new!"
- end
-
-end
